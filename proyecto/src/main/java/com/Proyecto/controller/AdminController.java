eef85f57823f4d1234d5c130044f9e4b04507898
MAOIRO17 <maoiro17@gmail.com>
2025-01-23 18:30:06 +0100

    Creacion de los primeras vistas

---
+++ proyecto/src/main/java/com/Proyecto/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.Proyecto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("")
+    public String PagPrincipal() {
+        return "admin/PagPrincipal";
+    }
+}
--- proyecto/src/main/java/com/Proyecto/model/DetalleCompra.java
+++ proyecto/src/main/java/com/Proyecto/model/DetalleCompra.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.Id;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,6 +25,6 @@ public class DetalleCompra {
 
     @OneToOne
     private Compra compra;
-    @OneToOne
+    @ManyToOne
     private Libros libros;
 }
